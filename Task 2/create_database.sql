830483a6c3f2c32e9c8b6f3053f6469a4d9e13d3
Casper Kristiansson <casper.kristiansson@yahoo.se>
2022-01-05 14:49:08 +0100

    Updated Task1,2

--- Task 2/create_database.sql
+++ Task 2/create_database.sql
@@ -12,12 +12,14 @@ ALTER TABLE classrooms ADD CONSTRAINT PK_classrooms PRIMARY KEY (room_id);
 CREATE TABLE instructor (
  instructor_id SERIAL NOT NULL,
  person_number VARCHAR(500) NOT NULL UNIQUE,
- address VARCHAR(500),
  name VARCHAR(500),
- teach_ensembles VARCHAR(500),
+ teach_ensembles BOOLEAN,
  age INT,
+ street VARCHAR(500),
  zip VARCHAR(500),
- city VARCHAR(500)
+ city VARCHAR(500),
+ email VARCHAR(500),
+ phone_number VARCHAR(100)
 );
 
 ALTER TABLE instructor ADD CONSTRAINT PK_instructor PRIMARY KEY (instructor_id);
@@ -25,7 +27,7 @@ ALTER TABLE instructor ADD CONSTRAINT PK_instructor PRIMARY KEY (instructor_id);
 
 CREATE TABLE instrument (
  id SERIAL NOT NULL,
- type VARCHAR(500) NOT NULL
+ type VARCHAR(500)
 );
 
 ALTER TABLE instrument ADD CONSTRAINT PK_instrument PRIMARY KEY (id);
@@ -33,7 +35,7 @@ ALTER TABLE instrument ADD CONSTRAINT PK_instrument PRIMARY KEY (id);
 
 CREATE TABLE instruments (
  instrument_id SERIAL NOT NULL,
- type VARCHAR(5000),
+ type VARCHAR(500) NOT NULL,
  brand VARCHAR(500),
  price INT,
  is_available BOOLEAN
@@ -58,7 +60,7 @@ CREATE TABLE student (
  student_id SERIAL NOT NULL,
  person_number VARCHAR(12) NOT NULL UNIQUE,
  name VARCHAR(500),
- address VARCHAR(500),
+ street VARCHAR(500),
  age INT,
  zip VARCHAR(500),
  city VARCHAR(500)
@@ -68,19 +70,19 @@ ALTER TABLE student ADD CONSTRAINT PK_student PRIMARY KEY (student_id);
 
 
 CREATE TABLE student_email (
- student_id SERIAL NOT NULL,
- email VARCHAR(500) NOT NULL
+ email VARCHAR(500) NOT NULL,
+ student_id SERIAL NOT NULL
 );
 
-ALTER TABLE student_email ADD CONSTRAINT PK_student_email PRIMARY KEY (student_id);
+ALTER TABLE student_email ADD CONSTRAINT PK_student_email PRIMARY KEY (email,student_id);
 
 
 CREATE TABLE student_phone (
- student_id SERIAL NOT NULL,
- phone_number VARCHAR(100) NOT NULL
+ phone_number VARCHAR(100) NOT NULL,
+ student_id SERIAL NOT NULL
 );
 
-ALTER TABLE student_phone ADD CONSTRAINT PK_student_phone PRIMARY KEY (student_id);
+ALTER TABLE student_phone ADD CONSTRAINT PK_student_phone PRIMARY KEY (phone_number,student_id);
 
 
 CREATE TABLE teacher_instrument (
@@ -93,9 +95,9 @@ ALTER TABLE teacher_instrument ADD CONSTRAINT PK_teacher_instrument PRIMARY KEY 
 
 CREATE TABLE lesson (
  lesson_id SERIAL NOT NULL,
- instructor_id INT,
+ instructor_id INT NOT NULL,
  room_id INT,
- price_type_id INT,
+ price_type_id INT NOT NULL,
  time TIMESTAMP(10) NOT NULL,
  genre VARCHAR(500),
  amount_students INT,
@@ -110,38 +112,38 @@ ALTER TABLE lesson ADD CONSTRAINT PK_lesson PRIMARY KEY (lesson_id);
 
 
 CREATE TABLE parent_email (
- student_id SERIAL NOT NULL,
- email VARCHAR(500) NOT NULL
+ email VARCHAR(500) NOT NULL,
+ student_id SERIAL NOT NULL
 );
 
-ALTER TABLE parent_email ADD CONSTRAINT PK_parent_email PRIMARY KEY (student_id);
+ALTER TABLE parent_email ADD CONSTRAINT PK_parent_email PRIMARY KEY (email,student_id);
 
 
 CREATE TABLE parent_phone (
- student_id SERIAL NOT NULL,
- phone_number VARCHAR(100) NOT NULL
+ phone_number VARCHAR(100) NOT NULL,
+ student_id SERIAL NOT NULL
 );
 
-ALTER TABLE parent_phone ADD CONSTRAINT PK_parent_phone PRIMARY KEY (student_id);
+ALTER TABLE parent_phone ADD CONSTRAINT PK_parent_phone PRIMARY KEY (phone_number,student_id);
 
 
 CREATE TABLE renting_instruments (
  rental_id SERIAL NOT NULL,
- time_rented TIMESTAMP(10),
- student_id INT,
- is_terminated BOOLEAN,
- instrument_id SERIAL
+ time_rented TIMESTAMP(10) NOT NULL,
+ student_id INT NOT NULL,
+ is_terminated BOOLEAN NOT NULL,
+ instrument_id INT NOT NULL
 );
 
 ALTER TABLE renting_instruments ADD CONSTRAINT PK_renting_instruments PRIMARY KEY (rental_id);
 
 
 CREATE TABLE sibling (
- student_id SERIAL NOT NULL,
- person_number VARCHAR(500) NOT NULL
+ person_number VARCHAR(500) NOT NULL,
+ student_id SERIAL NOT NULL
 );
 
-ALTER TABLE sibling ADD CONSTRAINT PK_sibling PRIMARY KEY (student_id);
+ALTER TABLE sibling ADD CONSTRAINT PK_sibling PRIMARY KEY (person_number,student_id);
 
 
 CREATE TABLE student_lesson (
@@ -162,9 +164,9 @@ ALTER TABLE teacher_instrument ADD CONSTRAINT FK_teacher_instrument_0 FOREIGN KE
 ALTER TABLE teacher_instrument ADD CONSTRAINT FK_teacher_instrument_1 FOREIGN KEY (id) REFERENCES instrument (id) ON DELETE CASCADE;
 
 
-ALTER TABLE lesson ADD CONSTRAINT FK_lesson_0 FOREIGN KEY (instructor_id) REFERENCES instructor (instructor_id) ON DELETE SET NULL;
+ALTER TABLE lesson ADD CONSTRAINT FK_lesson_0 FOREIGN KEY (instructor_id) REFERENCES instructor (instructor_id) ON DELETE NO ACTION;
 ALTER TABLE lesson ADD CONSTRAINT FK_lesson_1 FOREIGN KEY (room_id) REFERENCES classrooms (room_id) ON DELETE SET NULL;
-ALTER TABLE lesson ADD CONSTRAINT FK_lesson_2 FOREIGN KEY (price_type_id) REFERENCES price_scheme (price_type_id) ON DELETE SET NULL;
+ALTER TABLE lesson ADD CONSTRAINT FK_lesson_2 FOREIGN KEY (price_type_id) REFERENCES price_scheme (price_type_id) ON DELETE NO ACTION;
 
 
 ALTER TABLE parent_email ADD CONSTRAINT FK_parent_email_0 FOREIGN KEY (student_id) REFERENCES student (student_id) ON DELETE CASCADE;
@@ -173,8 +175,8 @@ ALTER TABLE parent_email ADD CONSTRAINT FK_parent_email_0 FOREIGN KEY (student_i
 ALTER TABLE parent_phone ADD CONSTRAINT FK_parent_phone_0 FOREIGN KEY (student_id) REFERENCES student (student_id) ON DELETE CASCADE;
 
 
-ALTER TABLE renting_instruments ADD CONSTRAINT FK_renting_instruments_0 FOREIGN KEY (student_id) REFERENCES student (student_id) ON DELETE CASCADE;
-ALTER TABLE renting_instruments ADD CONSTRAINT FK_renting_instruments_1 FOREIGN KEY (instrument_id) REFERENCES instruments (instrument_id) ON DELETE CASCADE;
+ALTER TABLE renting_instruments ADD CONSTRAINT FK_renting_instruments_0 FOREIGN KEY (student_id) REFERENCES student (student_id) ON DELETE NO ACTION;
+ALTER TABLE renting_instruments ADD CONSTRAINT FK_renting_instruments_1 FOREIGN KEY (instrument_id) REFERENCES instruments (instrument_id) ON DELETE NO ACTION;
 
 
 ALTER TABLE sibling ADD CONSTRAINT FK_sibling_0 FOREIGN KEY (student_id) REFERENCES student (student_id) ON DELETE CASCADE;
